836448b6f8c67d574c10b0ea4c7753a0786781a1
icpp <icpp@icpp.world>
2023-08-01 16:04:13 -0400

    PowerShell script to test all the canisters on Windows

---
+++ all_canisters.ps1
@@ -0,0 +1,40 @@
+#######################################################################
+# This is a Windows PowerShell script, 
+#
+# (-) equivalent to the Makefile target `all-canisters:
+# (-) In a Windows PowerShell (*):
+#
+#     .\all_canisters.ps1
+#
+# (*) The Miniconda Powershell is highly recommended
+#
+#######################################################################
+
+wsl --% dfx identity use default
+
+Write-Host " "
+Write-Host "--------------------------------------------------"
+Write-Host "Testing canisters\api_reference"
+Set-Location -Path .\canisters\api_reference
+.\demo.ps1
+
+Write-Host " "
+Write-Host "--------------------------------------------------"
+Write-Host "Testing canisters\counter"
+Set-Location -Path ..\counter
+.\demo.ps1
+
+Write-Host " "
+Write-Host "--------------------------------------------------"
+Write-Host "Testing canisters\counter4me"
+Set-Location -Path ..\counter4me
+.\demo.ps1
+
+Write-Host " "
+Write-Host "--------------------------------------------------"
+Write-Host "Testing canisters\counters"
+Set-Location -Path ..\counters
+.\demo.ps1
+
+# Change directory back to the root
+Set-Location -Path ..\..
